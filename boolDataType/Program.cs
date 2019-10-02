80b4a52c6ed2b94656970fc45d3f48e11f00487b
krummenCode <krummen.code@gmail.com>
2019-10-01 21:01:30 -0500

    Add project files.

---
+++ boolDataType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter a number
+            Console.WriteLine("Enter a positive or negative number");
+            int myNumber = Convert.ToInt32(Console.ReadLine());
+            
+            if (myNumber > 0)
+            {
+                Console.WriteLine("True, your number is greater than 0");
+            }
+            else
+            {
+                Console.WriteLine("Your number is not greater than 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
